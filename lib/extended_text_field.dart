ef937c5de63d84e46d76ab1b44ddb42b66a2eb10
hubaoqi <1006462702@qq.com>
2023-04-17 10:23:44 +0800

    fix:增加引用

--- lib/extended_text_field.dart
+++ lib/extended_text_field.dart
@@ -2,6 +2,7 @@ library extended_text_field;
 
 export 'package:extended_text_library/extended_text_library.dart';
 
+export 'src/extended_editable_text.dart';
 export 'src/extended_render_editable.dart';
 export 'src/extended_text_field.dart';
 export 'src/keyboard/binding.dart';
