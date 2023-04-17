27396b21eeecfe25039476b276fd1cd8d689f8d1
hubaoqi <1006462702@qq.com>
2023-04-17 10:18:13 +0800

    fix:同步系统代码2

--- lib/extended_text_field.dart
+++ lib/extended_text_field.dart
@@ -1,6 +1,8 @@
 library extended_text_field;
 
 export 'package:extended_text_library/extended_text_library.dart';
+
+export 'src/extended_render_editable.dart';
 export 'src/extended_text_field.dart';
 export 'src/keyboard/binding.dart';
 export 'src/keyboard/focus_node.dart';
